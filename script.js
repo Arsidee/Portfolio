bc0f73f7072b94b1ef5b5dbc458d4a7a3c5d3cee
Ryan Dilley <ryandilley54@outlook.com>
2026-05-23 13:18:25 -0500

    Updates

--- script.js
+++
@@ -1,9 +0,0 @@
-
-var nameInput = document.querySelector("#name-input");
-var greetingOutput = document.querySelector("#greeting-output");
-
-function doTheThing() {
-  var name = nameInput.value;
-
-  greetingOutput.innerHTML = "Hello " + name + ", welcome home!";
-}
